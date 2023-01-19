851babb4a383dffc01121a8bb30ee2a0e2ff5891
Juan Buitrago <juanbuitrago2002@outlook.com>
2023-01-19 10:28:07 -0500

    create de Home page and add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <div>
+        <h1>Logo</h1>
+        </div>
+        <div>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">Vegetarianos</a>
+          </li>
+          <li>
+            <a href="/contact">Platos Principales</a>
+          </li>
+          <li>
+            <a href="/contact">Tortas</a>
+          </li>
+          <li>
+            <a href="/contact">Comida Rápida </a>
+          </li>
+          <li>
+            <a href="/contact">Menú Niños</a>
+          </li>
+          <li>
+            <a href="/contact">Sopas</a>
+          </li>
+          </ul>
+        </div>
+       
+          </nav>
+    </header>
+  )
+}
