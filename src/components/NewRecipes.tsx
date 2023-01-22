7d54ffbe05a84dda3f24dd10f70b1c00766fc3aa
Juan Buitrago <juanbuitrago2002@outlook.com>
2023-01-22 00:12:17 -0500

    add animation

--- src/components/NewRecipes.tsx
+++ src/components/NewRecipes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 
 import defaultlImage from "../assets/Ojingeo-muchim-5.png";
 import startIcon from "../assets/icons/ic_star.svg";
@@ -7,6 +7,10 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+//icons
+import portionIcon from "../assets/icons/ic_portion.svg";
+import timeIcon from "../assets/icons/ic_time.svg";
+import difficultyIcon from "../assets/icons/ic_soup.svg";
 
 
 export default function NewRecipes() {
@@ -44,6 +48,8 @@ export default function NewRecipes() {
         }
       ]
       };
+      const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div className="new-recipes">
       <div className="recipes-title">
@@ -51,7 +57,11 @@ export default function NewRecipes() {
       </div>
       <div className="container-new-recipes">
       <Slider {...settings}>
-      <div className="new-recipe-item">
+      <div 
+      className={`new-recipe-item ${isHovered ? 'hovered' : ''}`}   
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      >
         <div className="recipe-image">
           <img src={defaultlImage} alt="recipe" loading="lazy" />
         </div>
@@ -80,10 +90,37 @@ export default function NewRecipes() {
                 loading="lazy"
               />
             </div>
+          </div>
+        </div>
+        <div className="recipe-info-back">
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={portionIcon} alt="" />
+            </div>
+            <p className="recipe-preparation-title">Tamaño de la porcion</p>
+            <p className="recipe-preparation-subtitle">4 raciones</p>
+          </div>
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={timeIcon} alt="" />
+            </div>
+            <p className="recipe-preparation-title">Tiempo de preparacion</p>
+            <p className="recipe-preparation-subtitle">10 minutos</p>
+          </div>
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={difficultyIcon} alt="" />
+            </div>
+            <p className="recipe-preparation-title">Dificultad</p>
+            <p className="recipe-preparation-subtitle">Facil</p>
           </div>
         </div>
       </div>
-      <div className="new-recipe-item">
+      <div 
+      className={`new-recipe-item ${isHovered ? 'hovered' : ''}`}   
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      >
         <div className="recipe-image">
           <img src={defaultlImage} alt="recipe" loading="lazy" />
         </div>
@@ -112,10 +149,37 @@ export default function NewRecipes() {
                 loading="lazy"
               />
             </div>
+          </div>
+        </div>
+        <div className="recipe-info-back">
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={portionIcon} alt="" />
+            </div>
+            <p className="recipe-preparation-title">Tamaño de la porcion</p>
+            <p className="recipe-preparation-subtitle">4 raciones</p>
+          </div>
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={timeIcon} alt="" />
+            </div>
+            <p className="recipe-preparation-title">Tiempo de preparacion</p>
+            <p className="recipe-preparation-subtitle">10 minutos</p>
+          </div>
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={difficultyIcon} alt="" />
+            </div>
+            <p className="recipe-preparation-title">Dificultad</p>
+            <p className="recipe-preparation-subtitle">Facil</p>
           </div>
         </div>
       </div>
-      <div className="new-recipe-item">
+      <div 
+      className={`new-recipe-item ${isHovered ? 'hovered' : ''}`}   
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      >
         <div className="recipe-image">
           <img src={defaultlImage} alt="recipe" loading="lazy" />
         </div>
@@ -144,10 +208,37 @@ export default function NewRecipes() {
                 loading="lazy"
               />
             </div>
+          </div>
+        </div>
+        <div className="recipe-info-back">
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={portionIcon} alt="" />
+            </div>
+            <p className="recipe-preparation-title">Tamaño de la porcion</p>
+            <p className="recipe-preparation-subtitle">4 raciones</p>
+          </div>
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={timeIcon} alt="" />
+            </div>
+            <p className="recipe-preparation-title">Tiempo de preparacion</p>
+            <p className="recipe-preparation-subtitle">10 minutos</p>
+          </div>
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={difficultyIcon} alt="" />
+            </div>
+            <p className="recipe-preparation-title">Dificultad</p>
+            <p className="recipe-preparation-subtitle">Facil</p>
           </div>
         </div>
       </div>
-      <div className="new-recipe-item">
+      <div 
+      className={`new-recipe-item ${isHovered ? 'hovered' : ''}`}   
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      >
         <div className="recipe-image">
           <img src={defaultlImage} alt="recipe" loading="lazy" />
         </div>
@@ -178,37 +269,27 @@ export default function NewRecipes() {
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="new-recipe-item">
-        <div className="recipe-image">
-          <img src={defaultlImage} alt="recipe" loading="lazy" />
-        </div>
-        <div className="recipe-info">
-          <div className="container-recipe-title">
-            <h3 className="title-name">title</h3>
-            <h3 className="title-product"> Title</h3>
+        <div className="recipe-info-back">
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={portionIcon} alt="" />
+            </div>
+            <p className="recipe-preparation-title">Tamaño de la porcion</p>
+            <p className="recipe-preparation-subtitle">4 raciones</p>
           </div>
-          <div className="recipe-ratings">
-            <div className="score">
-              <img
-                src={startIcon}
-                width={23}
-                height={23}
-                alt="star icon"
-                loading="lazy"
-              />
-              <p>5.0</p>
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={timeIcon} alt="" />
             </div>
-            <div className="like">
-              <img
-                src={likeIcon}
-                width={23}
-                height={23}
-                alt="star icon"
-                loading="lazy"
-              />
+            <p className="recipe-preparation-title">Tiempo de preparacion</p>
+            <p className="recipe-preparation-subtitle">10 minutos</p>
+          </div>
+          <div className="item-recipe-preparation">
+            <div className="container-img">
+              <img src={difficultyIcon} alt="" />
             </div>
+            <p className="recipe-preparation-title">Dificultad</p>
+            <p className="recipe-preparation-subtitle">Facil</p>
           </div>
         </div>
       </div>
